e1270afdfe7698ea9b576617f4982c0f79f209d8
Miloud Maamar <development@maamar.fr>
2016-06-02 23:46:35 +0200

    Added Users location + infowindow

--- distant/userlocation.php
+++ distant/userlocation.php
@@ -59,6 +59,10 @@ if(isset($_POST['storingLoc'])){
 }
 
 
+
+
+
+
 if(isset($_POST['getFriendsLoc'])){
 
     $val = $_POST['positions'];
@@ -69,25 +73,25 @@ if(isset($_POST['getFriendsLoc'])){
     $data['autres']=array();
 
     //REQUETE ID COLLABS PROJET.
-    $id_collabs = array();
+    //$id_collabs = array();
 
-    $req  = "SELECT DISTINCT ";
-    $req .= "	aut.id_auteur";
-    $req .= "FROM ";
-    $req .= "	spip_auteurs as aut ";
-    $req .= "	LEFT JOIN spip_auteurs_liens as autl ON aut.id_auteur = autl.id_auteur ";
-    $req .= "	LEFT JOIN spip_articles as art ON autl.id_objet = art.id_article ";
-    $req .= "	LEFT JOIN appli_safety as sf ON sf.id_voyageur = aut.id_auteurs ";
-    $req .= "WHERE ";
-    $req .= "	art.id_secteur = 16";
-    $req .= "    AND art.statut = 'publie'";
-    $req .= "    AND sf.shareLoc = 1";
-    $req .= "    AND aut.id_auteur != $id";
+    //$req  = "SELECT DISTINCT ";
+    //$req .= "	aut.id_auteur";
+    //$req .= "FROM ";
+    //$req .= "	spip_auteurs as aut ";
+    //$req .= "	LEFT JOIN spip_auteurs_liens as autl ON aut.id_auteur = autl.id_auteur ";
+    //$req .= "	LEFT JOIN spip_articles as art ON autl.id_objet = art.id_article ";
+    //$req .= "	LEFT JOIN appli_safety as sf ON sf.id_voyageur = aut.id_auteurs ";
+    //$req .= "WHERE ";
+    //$req .= "	art.id_secteur = 16";
+    //$req .= "    AND art.statut = 'publie'";
+    //$req .= "    AND sf.shareLoc = 1";
+    //$req .= "    AND aut.id_auteur != $id";
 
-    $request = mysqli_query($db,$req);
-    while($res = mysqli_fetch_assoc($request)){
-        array_push($id_collabs,$res);
-    }
+    //$request = mysqli_query($db,$req);
+    //while($res = mysqli_fetch_assoc($request)){
+    //    array_push($id_collabs,$res);
+    //}
 
 
     //REQUETES DONNEES DE LOCALISATION GLOBALES
@@ -101,10 +105,6 @@ if(isset($_POST['getFriendsLoc'])){
     $req .= "WHERE ";
     $req .= "    sf.shareLoc = 1";
     $req .= "    AND aut.id_auteur != $id";
-    foreach($id_collabs as $value)
-    {
-        $req .= "AND aut.id_auteur != $value";
-    }
 
     $selectAll = mysqli_query($db,$req);
     while ($_5pos = mysqli_fetch_assoc($selectAll)){
@@ -114,30 +114,78 @@ if(isset($_POST['getFriendsLoc'])){
 
     //REQUETE DONNEES DE LOCALISATION COLLABS
 
-    $req  = "SELECT DISTINCT ";
-    $req .= "	*";
-    $req .= "FROM ";
-    $req .= "	spip_auteurs as aut ";
-    $req .= "	LEFT JOIN appli_tracing as tr ON tr.id_voyageur = aut.id_auteurs ";
-    $req .= "	LEFT JOIN appli_safety as sf ON sf.id_voyageur = aut.id_auteurs ";
-    $req .= "WHERE ";
-    $req .= "    sf.shareLoc = 1";
-    $req .= "    AND aut.id_auteur != $id";
-    foreach($id_collabs as $value)
-    {
-        $req .= "AND aut.id_auteur = $value";
-    }
-    $selectCol = mysqli_query($db,$req);
+    //$req  = "SELECT DISTINCT ";
+    //$req .= "	*";
+    //$req .= "FROM ";
+    //$req .= "	spip_auteurs as aut ";
+    //$req .= "	LEFT JOIN appli_tracing as tr ON tr.id_voyageur = aut.id_auteurs ";
+    //$req .= "	LEFT JOIN appli_safety as sf ON sf.id_voyageur = aut.id_auteurs ";
+    //$req .= "WHERE ";
+    //$req .= "    sf.shareLoc = 1";
+    //$req .= "    AND aut.id_auteur != $id";
+    //foreach($id_collabs as $value)
+    //{
+    //    $req .= "AND aut.id_auteur = $value";
+    //}
+    //$selectCol = mysqli_query($db,$req);
 
-    while ($_5pos = mysqli_fetch_assoc($selectCol)){
-        array_push($data['project'],$_5pos);
-    }
+    //while ($_5pos = mysqli_fetch_assoc($selectCol)){
+    //    array_push($data['project'],$_5pos);
+    //}
 
     //echo $ins;
     echo json_encode($data);
 
 }
 
+
+
+if(isset($_POST['usersPos'])){
+
+    $id = $_POST['id_voyageur'];
+
+    $data = array();
+    $data["project"]=array();
+    $data["autres"]=array();
+
+    $array = array();
+
+    //REQUETES DONNEES DE LOCALISATION GLOBALES
+
+    $req  = "SELECT DISTINCT ";
+    $req .= "	aut.id_auteur, aut.login, tr.serie5 ";
+    $req .= "FROM ";
+    $req .= "	spip_auteurs AS aut ";
+    $req .= "	LEFT JOIN appli_tracing AS tr ON tr.id_voyageur = aut.id_auteur ";
+    $req .= "	LEFT JOIN appli_safety AS sf ON sf.id_voyageur = aut.id_auteur ";
+    $req .= "WHERE ";
+    $req .= "    sf.shareLoc = 1 ";
+    $req .= "    AND aut.id_auteur != $id ";
+    $req .= "ORDER BY tr.id DESC ";
+
+
+    $selectAll = mysqli_query($db,$req);
+    while ($_5pos = mysqli_fetch_assoc($selectAll)){
+
+        $login = $_5pos["login"];
+        ////var_dump($_5pos["login"]);
+        //echo $login;
+        if(array_key_exists($login,$array)){
+
+        }
+        else{
+            $array[$login] = array();
+            $array[$login] = $_5pos;
+        }
+    }
+    foreach ($array as $value){
+        array_push($data["autres"],$value);
+    }
+    //array_push($data['autres'],$_5pos);
+    //var_dump ($array);
+    echo json_encode($data);
+
+}
 
 
 //if(isset($_GET['testStoringLoc'])){
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -90,7 +90,7 @@ angular.module('vayaterra.controllers', ['uiGmapgoogle-maps', 'LocalForageModule
                     $rootScope.userdata.shareLoc = ((data.message.shareLoc !== '0' && data.message.shareLoc !== '1') ? false : Boolean(data.message.shareLoc));
                     $rootScope.userdata.safetyLoc = ((data.message.safetyLoc !== '0' && data.message.safetyLoc !== '1') ? false : Boolean(data.message.safetyLoc));
                     $rootScope.userdata.logged = true;
-                    data.message.img = $rootScope.userdata.img = getProfileImg();
+                    data.message.img = $rootScope.userdata.img = $rootScope.getProfileImg($rootScope.userdata.id_auteur);
                     //console.log($rootScope.userdata);
                     $rootScope.user.setItem('data', data.message).then(function () {
                         $scope.closeLogin();
@@ -107,8 +107,8 @@ angular.module('vayaterra.controllers', ['uiGmapgoogle-maps', 'LocalForageModule
 
     //}
 
-    var getProfileImg = function () {
-        var path = $rootScope.rootUrl + "IMG/auton" + $rootScope.userdata.id_auteur;
+    $rootScope.getProfileImg = function (idauteur) {
+        var path = $rootScope.rootUrl + "IMG/auton" + idauteur;
         var def = $rootScope.rootUrl + "IMG/autondefault.jpg";
         if (testImg(path + '.jpg')) {
             return path + '.jpg';
@@ -228,7 +228,7 @@ angular.module('vayaterra.controllers', ['uiGmapgoogle-maps', 'LocalForageModule
         $rootScope.fivelastpos = [];
         var watchOptions = {
             timeout: 30000,
-            enableHighAccuracy: false // may cause errors if true
+            enableHighAccuracy: true // may cause errors if true
         };
         $rootScope.SGL = $cordovaGeolocation.watchPosition(watchOptions);
         $rootScope.SGL.then(
@@ -498,21 +498,7 @@ angular.module('vayaterra.controllers', ['uiGmapgoogle-maps', 'LocalForageModule
             fn: 'OpenAddPoi()',
             show: 'logged',
             hide: '',
-        },
-        {
-            name: 'add event',
-            cssClass: 'addPoi',
-            fn: 'OpenAddPoi()',
-            show: 'logged',
-            hide: '',
-        },
-        {
-            name: 'update poi',
-            cssClass: 'addPoi',
-            fn: 'updatePoi()',
-            show: '',
-            hide: '',
-        },
+        }
     ];
 
     //initialisation des paramètres de la carte
@@ -581,6 +567,63 @@ angular.module('vayaterra.controllers', ['uiGmapgoogle-maps', 'LocalForageModule
         //console.log('markpoi');
         //console.log($scope.poilist);
     };
+
+    //Fonction qui récupère les positions utilsateurs
+
+    var getUsersPos = function () {
+        var post = {};
+        post.usersPos = true;
+        post.id_voyageur = $rootScope.userdata.id_auteur;
+        //console.log($.param(post));
+        $http({
+            method: 'POST',
+            url: $rootScope.disturl + 'userlocation.php',
+            data: $.param(post), // pass in data as strings
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            }
+        }).success(function (data) {
+            addUsersonMap(data.autres);
+        });
+    }
+
+    var addUsersonMap = function (data) {
+
+        $scope.usersPos = [];
+        //console.log(data);
+        data.forEach(function (obj) {
+
+            obj.serie5 = JSON.parse(obj.serie5);
+
+            var icon = {
+                url: $rootScope.getProfileImg(obj.id_auteur),
+                scaledSize : new google.maps.Size(30,30),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(0,0),
+            }
+
+            var user = {
+                idkey: '888' + obj.id_auteur,
+                nom: obj.login,
+                pos: {
+                    latitude: obj.serie5[obj.serie5.length - 1].latitude,
+                    longitude: obj.serie5[obj.serie5.length - 1].longitude
+                },
+                options : {
+                    icon: icon
+                }
+
+            }
+
+            $scope.usersPos.push(user);
+        })
+        
+        //console.log($scope.usersPos);
+
+
+
+    }
+
 
     var updatePoi = function (data, callback) {
 
@@ -771,10 +814,11 @@ angular.module('vayaterra.controllers', ['uiGmapgoogle-maps', 'LocalForageModule
     //Fonction de suivi géolocalisé, met à jour la carte avec les dernières infos de l'utilisateur
     $scope.currentPos = function () {
         currentHeading();
+        getUsersPos();
         //$scope.closeAddPoi();
         var posOptions = {
             timeout: 10000,
-            enableHighAccuracy: false
+            enableHighAccuracy: true
         };
         $cordovaGeolocation
             .getCurrentPosition(posOptions)
@@ -799,8 +843,8 @@ angular.module('vayaterra.controllers', ['uiGmapgoogle-maps', 'LocalForageModule
 
         watchHeading();
         var watchOptions = {
-            timeout: 100,
-            enableHighAccuracy: false // may cause errors if true
+            timeout: 1000,
+            enableHighAccuracy: true // may cause errors if true
         };
         $scope.watch = $cordovaGeolocation.watchPosition(watchOptions);
         $scope.isWatching = true;
@@ -822,6 +866,7 @@ angular.module('vayaterra.controllers', ['uiGmapgoogle-maps', 'LocalForageModule
                 //On actualise le centre de la carte
                 $scope.map.center.latitude = position.coords.latitude;
                 $scope.map.center.longitude = position.coords.longitude;
+
             });
     };
 
